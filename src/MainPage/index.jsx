306a76a8ab2dccd519b29f625c27dde8e885a604
Nick Brian <nbjm1234@outlook.com>
2023-01-31 00:44:42 -0600

    hoover bonito de card terminado

--- src/MainPage/index.jsx
+++ src/MainPage/index.jsx
@@ -53,7 +53,6 @@ const IndexMainPage = () => {
                 setIsSearching={setIsSearching}
                 />
             </nav>
-
             <div 
             style={{
                 width: "100%",
